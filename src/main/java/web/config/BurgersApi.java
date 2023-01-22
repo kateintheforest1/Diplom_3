83a4270c784df036692c23f5be3312b6d98a924f
Ekaterina Kuznetsova <108665826+kateintheforest1@users.noreply.github.com>
2023-01-22 17:42:38 +0500

    add Base page

---
+++ src/main/java/web/config/BurgersApi.java
@@ -0,0 +1,12 @@
+package web.config;
+
+public interface BurgersApi {
+    String BASE_URL = "https://stellarburgers.nomoreparties.site";
+
+    String MAIN_PAGE_PATH = "/";
+    String LOGIN_PATH = "/login";
+    String FORGOT_PASSWORD_PAGE_PATH = "/forgot-password";
+    String PROFILE_PAGE_PATH = "/account/profile";
+    String REGISTER_PAGE_PATH = "/register";
+}
+
